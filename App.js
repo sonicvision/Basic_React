82eeee924d40f017f6fa7cd8a0fe491c5bf51abb
Praveen <>
2018-10-18 04:50:41 +0530

    Map Added

--- App.js
+++ App.js
@@ -6,45 +6,97 @@
  * @flow
  */
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import React, { Component } from "react";
+import { Platform, StyleSheet, Text, View, Image } from "react-native";
+import MapView, { Marker } from "react-native-maps";
 
 const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+  ios: "Press Cmd+R to reload,\n" + "Cmd+D or shake for dev menu",
   android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
+    "Double tap R on your keyboard to reload,\n" +
+    "Shake or press menu button for dev menu"
 });
-
+const markers = [
+  {
+    title: "Coffee 1",
+    description: "Good Coffee",
+    latlng: {
+      latitude: 37.78825,
+      longitude: -122.4324
+    }
+  },
+  {
+    title: "Coffee 2",
+    description: "Good Coffee",
+    latlng: {
+      latitude: 37.79995,
+      longitude: -122.4334
+    }
+  }
+];
 //type Props = {};
 export default class App extends React.Component {
+  state = {
+    region: {
+      latitude: 37.78825,
+      longitude: -122.4324,
+      latitudeDelta: 0.0922,
+      longitudeDelta: 0.0421
+    },
+    markers: markers
+  };
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React  Ok Native!</Text>
-        <Text style={styles.instructions}>To get iououo started,  Ok edit App.js</Text>
+        <View style={styles.mapContainer}>
+          <MapView style={styles.mapView} initialRegion={this.state.region}>
+            {this.state.markers.map(marker => (
+              <Marker
+                coordinate={marker.latlng}
+                title={marker.title}
+                description={marker.description}
+              >
+                <Image
+                  style={{ height: 50, width: 50 }}
+                  source={require("./assets/coffee-icon.png")}
+                />
+              </Marker>
+            ))}
+          </MapView>
+        </View>
+        <Text style={styles.welcome}>Welcome to React Ok Native!</Text>
+        <Text style={styles.instructions}>
+          To get iououo started, Ok edit App.jsczczczxcxzczxczxczxczxczxc
+        </Text>
         <Text style={styles.instructions}>{instructions}</Text>
       </View>
     );
   }
 }
 
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F5FCFF"
+  },
+  mapContainer: {
+    flex: 1,
+    flexDirection: "row",
+    alignItems: "stretch"
+  },
+  mapView: {
+    flex: 1
   },
   welcome: {
     fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+    textAlign: "center",
+    margin: 10
   },
   instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    textAlign: "center",
+    color: "#333333",
+    marginBottom: 5
+  }
 });
